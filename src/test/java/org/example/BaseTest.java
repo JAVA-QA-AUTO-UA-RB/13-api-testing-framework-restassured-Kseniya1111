435bee1ac05cd91e3aaec4c24dd2b4e59a069edd
Kseniya1111 <abbasovakm@gmail.com>
2025-11-07 21:02:16 +0100

    Update BaseTest.Java

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,13 +1,11 @@
 package org.example;
 
-
-
 import io.restassured.RestAssured;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeClass;
 
 import static io.restassured.RestAssured.given;
 
@@ -15,18 +13,16 @@ public class BaseTest {
     protected static final String BASE_URI = "https://jsonplaceholder.typicode.com";
     protected RequestSpecification requestSpec;
 
-    @BeforeMethod
+    @BeforeClass
     public void commonSetup() {
-        if (requestSpec == null) {
-            RestAssured.baseURI = BASE_URI;
-            RestAssured.filters(
-                    new RequestLoggingFilter(LogDetail.ALL),
-                    new ResponseLoggingFilter(LogDetail.ALL)
-            );
+        RestAssured.baseURI = BASE_URI;
+        RestAssured.filters(
+                new RequestLoggingFilter(LogDetail.ALL),
+                new ResponseLoggingFilter(LogDetail.ALL)
+        );
 
-            requestSpec = given()
-                    .baseUri(BASE_URI)
-                    .header("Content-Type", "application/json");
-        }
+        requestSpec = given()
+                .baseUri(BASE_URI)
+                .header("Content-Type", "application/json");
     }
 }
